e3d988cb95ee82a6a78755682d01d206e94a4ce1
Fryntiz <fryntiz@gmail.com>
2016-11-04 12:42:01 +0100

    Renombrado html a php

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE HTML>
+<HTML lang="es">
+	<HEAD>
+		<META charset="utf-8"/>
+		<TITLE>Fryntiz Desarrollador WEB</TITLE>
+		<META name="description" content="Curriculum online de Raúl Caro Pastorino alias 'Fryntiz' donde se exponen sus aptitudes, trayectoria en formación y laboral como desarrollador web"/>
+		<META name="keywords" content="curriculum, fryntiz, programador, desarrollador, página web, web, online, curriculum online, desarrollador web, sitio web, raul, raul caro pastorino, Raúl Caro Pastorino, Raúl, Raúl Caro, Caro Pastorino, web developer, developer, crear página, crear página web, hacer página web"/>
+		<META name="author" content="Raúl Caro Pastorino"/>
+		<LINK rel="shortcut icon" href="./images/favicon.png"/>
+		<LINK rel="stylesheet" href="./CSS/styles.css"/>
+		<SCRIPT src="JS/scripts.js"></SCRIPT>
+	</HEAD>
+
+	<BODY>
+		<DIV>
+
+		</DIV>
+
+		<FOOTER id="CajaFooter">
+			<ADDRESS>
+				Fryntiz - tecnico@fryntiz.es
+			</ADDRESS>
+		</FOOTER>
+	</BODY>
+</HTML>
