c875cc8dd65dfde7d27f1e14d39039b6e3a08dec
Fryntiz <fryntiz@gmail.com>
2016-11-04 21:26:33 +0100

    Añadida presentación

--- titulo.php
+++ titulo.php
@@ -16,3 +16,15 @@
 <DIV id="subtitulo">
 
 </DIV>
+
+<DIV id="presentacion">
+	<SPAN style="color:#688cc7;">WEb oficial de</SPAN> <SPAN style="color:#1d53c7;">Fryntiz</SPAN>
+	<BR/>
+	<SPAN style="color:#be2e00;">www.fryntiz.es</SPAN>
+	<BR/>
+	<SPAN style="color:#688cc7;">Desarrollador</SPAN> <SPAN style="color:#68d162;">WEB</SPAN>
+	<BR/>
+	<SPAN style="color:#f98500;">Técnico</SPAN> <SPAN style="color:#688cc7;">en Sistemas</SPAN>
+	<BR/>
+	<SPAN style="color:#f98500;">Técnico</SPAN> <SPAN style="color:#688cc7;">en Redes</SPAN>
+</DIV>
