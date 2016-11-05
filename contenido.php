9c05d59c11b400f08992573ae12a42f7961b2256
Fryntiz <fryntiz@gmail.com>
2016-11-05 02:08:58 +0100

    Añadido enlace hacia curriculum

---
+++ contenido.php
@@ -0,0 +1,5 @@
+<A class="A_sinDecorar" href="http://www.curriculum.fryntiz.es" title="Curriculum Raúl Caro Pastorino - Fryntiz" target="_blank">
+	<DIV id="verCurriculum">
+		<P>VER CURRICULUM</P>
+	</DIV>
+</A>
