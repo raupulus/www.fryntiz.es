405e2162ee57d706abecf7d84f81bcc28c3f307a
Fryntiz <fryntiz@gmail.com>
2016-11-06 01:32:35 +0100

    Agregada base para 3 entradas de proyectos donde colaboro

--- contenido.php
+++ contenido.php
@@ -9,6 +9,42 @@
 	<H2>PROYECTOS DONDE COLABORO</H2>
 	<DIV class="cajaMiniLogo"><DIV class="miniLogo"></DIV></DIV>
 	<DIV class="cajaContenido">
+		<DIV class="cajaEntrada">
+			<DIV class="imagenEntrada">
+				<A href="http://www.laguialinux.es" title="La Guía Linux" target="_blank"><IMG src="./images/laguialinux.png" alt="La Guía Linux"/></A>
+			</DIV>
+
+			<DIV class="detallesEntrada">
+				<H3>La Guía Linux</H3>
+				<HR/>
+				<A href="http://www.laguialinux.es" title="La Guía Linux" class="enlaceEntrada">www.laguialinux.es</A>
+				<P>
+					Sitio WEB con Guías y vídeotutoriales sobre informática, especialmente sobre el Sistema Operativo Linux y el software libre.
+				</P>
+			</DIV>
+		</DIV>
+
+		<DIV class="cajaEntrada">
+			<DIV class="imagenEntrada">
+				<IMG src="./images/desdechipiona.png" alt="Desde Chipiona"/>
+			</DIV>
+
+			<DIV class="detallesEntrada">
+
+			</DIV>
+		</DIV>
+
+		<DIV class="cajaEntrada">
+			<DIV class="imagenEntrada">
+				<IMG src="./images/nzcreaciones.png" alt="NZ Creaciones"/>
+			</DIV>
+
+			<DIV class="detallesEntrada">
+
+			</DIV>
+		</DIV>
+
+
 
 	</DIV>
 	</ARTICLE>
