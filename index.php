3a338f0effcc95ff8e73af9fed6107b236954dd3
Fryntiz <fryntiz@gmail.com>
2016-11-06 21:08:54 +0100

    Añadido titulo, descripción e imagen para las redes sociales

--- index.php
+++ index.php
@@ -3,9 +3,26 @@
 	<HEAD>
 		<META charset="utf-8"/>
 		<TITLE>Fryntiz Desarrollador WEB</TITLE>
-		<META name="description" content="Curriculum online de Raúl Caro Pastorino alias 'Fryntiz' donde se exponen sus aptitudes, trayectoria en formación y laboral como desarrollador web"/>
+		<META name="description" content="Fryntiz Desarrollador de aplicaciones WEB, página oficial"/>
 		<META name="keywords" content="curriculum, fryntiz, programador, desarrollador, página web, web, online, curriculum online, desarrollador web, sitio web, raul, raul caro pastorino, Raúl Caro Pastorino, Raúl, Raúl Caro, Caro Pastorino, web developer, developer, crear página, crear página web, hacer página web"/>
 		<META name="author" content="Raúl Caro Pastorino"/>
+<!--Metaetiquetas para Facebook-->
+		<META property="og:title" content="Fryntiz Desarrollador WEB"/>
+		<META property="og:description" content="Sitio WEB Oficial de Raúl Caro Pastorino (Fryntiz)" />
+		<META property="og:image" content="./images/LogoFryntiz.png"/>
+<!--Metaetiquetas para Twitter-->
+		<META name="twitter:title" content="Fryntiz Desarrollador WEB"/>
+		<META name="twitter:description" content="Sitio WEB Oficial de Raúl Caro Pastorino (Fryntiz)" />
+		<META name="twitter:image:src" content="./images/LogoFryntiz.png"/>
+		<META name="twitter:site" content="@fryntiz"/>
+		<META name="twitter:creator" content="@fryntiz"/>
+<!--Metaetiquetas para Google+-->
+		<LINK href="https://plus.google.com/109655402661721084499/posts" rel="author"/>
+		<LINK href="https://plus.google.com/109655402661721084499" rel="publisher"/>
+		<META itemprop="name" content="Fryntiz Desarrollador WEB"/>
+		<META itemprop="description" content="Sitio WEB Oficial de Raúl Caro Pastorino (Fryntiz)" />
+		<META itemprop="image" content="./images/LogoFryntiz.png"/>
+<!--Enlaces de archivos que componen el sitio-->
 		<LINK rel="shortcut icon" href="./images/favicon.png"/>
 		<LINK rel="stylesheet" href="./CSS/styles.css"/>
 		<LINK rel="stylesheet" href="./CSS/titulo.css"/>
