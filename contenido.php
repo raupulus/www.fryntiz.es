a17dac0805c9379f341981428449fe6089f3073b
Fryntiz <fryntiz@gmail.com>
2017-10-20 00:04:32 +0200

    Mejorado espaciado

--- contenido.php
+++ contenido.php
@@ -1,94 +1,94 @@
 
-	<DIV id="verCurriculum">
-		<A class="A_sinDecorar" href="http://www.curriculum.fryntiz.es" title="Curriculum Raúl Caro Pastorino - Fryntiz" target="_blank"><P>VER CURRICULUM</P></A>
-	</DIV>
+    <DIV id="verCurriculum">
+        <A class="A_sinDecorar" href="http://www.curriculum.fryntiz.es" title="Curriculum Raúl Caro Pastorino - Fryntiz" target="_blank"><P>VER CURRICULUM</P></A>
+    </DIV>
 
 
 <!--
 -------------------------------
-Proyectos
+         Proyectos
 -------------------------------
 -->
 <SECTION>
-	<ARTICLE>
-	<H2>PROYECTOS DONDE COLABORO</H2>
-	<DIV class="cajaMiniLogo"><DIV class="miniLogo"></DIV></DIV>
-	<DIV class="cajaContenido">
-		<DIV class="cajaEntrada">
-			<DIV class="imagenEntrada">
-				<A href="http://www.laguialinux.es" title="La Guía Linux" target="_blank"><IMG src="./images/laguialinux.png" alt="La Guía Linux"/></A>
-			</DIV>
+    <ARTICLE>
+    <H2>Colaboraciones Proyectos WEB</H2>
+    <DIV class="cajaMiniLogo"><DIV class="miniLogo"></DIV></DIV>
+    <DIV class="cajaContenido">
+        <DIV class="cajaEntrada">
+            <DIV class="imagenEntrada">
+                <A href="http://www.laguialinux.es" title="La Guía Linux" target="_blank"><IMG src="./images/laguialinux.png" alt="La Guía Linux"/></A>
+            </DIV>
 
-			<DIV class="detallesEntrada">
-				<H3>La Guía Linux</H3>
-				<HR/>
-				<A href="http://www.laguialinux.es" title="La Guía Linux" class="enlaceEntrada">www.laguialinux.es</A>
-				<P>
-					Sitio WEB con Guías y vídeotutoriales sobre informática, especialmente sobre Linux.
-				</P>
-			</DIV>
-		</DIV>
+            <DIV class="detallesEntrada">
+                <H3>La Guía Linux</H3>
+                <HR/>
+                <A href="http://www.laguialinux.es" title="La Guía Linux" class="enlaceEntrada">www.laguialinux.es</A>
+                <P>
+                    Sitio WEB con Guías y vídeotutoriales sobre informática, especialmente sobre Linux.
+                </P>
+            </DIV>
+        </DIV>
 
-		<DIV class="cajaEntrada">
-			<DIV class="imagenEntrada">
-				<A href="http://www.desdechipiona.es" title="Desde Chipiona" target="_blank"><IMG src="./images/desdechipiona.png" alt="Desde Chipiona"/></A>
-			</DIV>
+        <DIV class="cajaEntrada">
+            <DIV class="imagenEntrada">
+                <A href="http://www.desdechipiona.es" title="Desde Chipiona" target="_blank"><IMG src="./images/desdechipiona.png" alt="Desde Chipiona"/></A>
+            </DIV>
 
-			<DIV class="detallesEntrada">
-				<H3>Desde Chipiona</H3>
-				<HR/>
-				<A href="http://www.desdechipiona.es" title="Desde Chipiona" class="enlaceEntrada">www.desdechipiona.es</A>
-				<P>
-					Proyecto de sitio WEB con información y Galería de imágenes sobre Chipiona
-				</P>
-			</DIV>
-		</DIV>
+            <DIV class="detallesEntrada">
+                <H3>Desde Chipiona</H3>
+                <HR/>
+                <A href="http://www.desdechipiona.es" title="Desde Chipiona" class="enlaceEntrada">www.desdechipiona.es</A>
+                <P>
+                    Proyecto de sitio WEB con información y Galería de imágenes sobre Chipiona
+                </P>
+            </DIV>
+        </DIV>
 
-		<DIV class="cajaEntrada">
-			<DIV class="imagenEntrada">
-				<A href="http://www.nzweb.org" title="NZ Creaciones" target="_blank"><IMG src="./images/nzcreaciones.png" alt="NZ Creaciones"/></A>
-			</DIV>
+        <DIV class="cajaEntrada">
+            <DIV class="imagenEntrada">
+                <A href="http://www.nzweb.org" title="NZ Creaciones" target="_blank"><IMG src="./images/nzcreaciones.png" alt="NZ Creaciones"/></A>
+            </DIV>
 
-			<DIV class="detallesEntrada">
-				<H3>NZ Creaciones</H3>
-				<HR/>
-				<A href="http://www.nzcreaciones.es" title="NZ Creaciones" class="enlaceEntrada">www.nzcreaciones.es</A>
-				<P>
-					Proyecto que engloba otros proyectos de animación gráfica, entretenimiento, risas y ocio.
-				</P>
-			</DIV>
-		</DIV>
-	</DIV>
-	</ARTICLE>
+            <DIV class="detallesEntrada">
+                <H3>NZ Creaciones</H3>
+                <HR/>
+                <A href="http://www.nzcreaciones.es" title="NZ Creaciones" class="enlaceEntrada">www.nzcreaciones.es</A>
+                <P>
+                    Proyecto que engloba otros proyectos de animación gráfica, entretenimiento, risas y ocio.
+                </P>
+            </DIV>
+        </DIV>
+    </DIV>
+    </ARTICLE>
 </SECTION>
 
 <!--
 -------------------------------
-Sitios WEB Creados
+      Sitios WEB Creados
 -------------------------------
 -->
 
 <!--
 <SECTION>
-	<ARTICLE>
-	<H2>SITIOS WEB CREADOS</H2>
-	<DIV class="cajaMiniLogo"><DIV class="miniLogo"></DIV></DIV>
-	<DIV class="cajaContenido">
-		<DIV class="cajaEntrada">
-			<DIV class="imagenEntrada">
-				<A href="#" title="Nombre Empresa" target="_blank"><IMG src="./images/PlantillaImágenes.png" alt="Nombre Empresa"/></A>
-			</DIV>
+    <ARTICLE>
+    <H2>SITIOS WEB CREADOS</H2>
+    <DIV class="cajaMiniLogo"><DIV class="miniLogo"></DIV></DIV>
+    <DIV class="cajaContenido">
+        <DIV class="cajaEntrada">
+            <DIV class="imagenEntrada">
+                <A href="#" title="Nombre Empresa" target="_blank"><IMG src="./images/PlantillaImágenes.png" alt="Nombre Empresa"/></A>
+            </DIV>
 
-			<DIV class="detallesEntrada">
-				<H3>Nombre Empresa</H3>
-				<HR/>
-				<A href="#" title="Nombre Empresa" class="enlaceEntrada">www.empresa.es</A>
-				<P>
-					Descripción de la Empresa y trabajo realizado en el sitio web
-				</P>
-			</DIV>
-		</DIV>
-	</DIV>
-	</ARTICLE>
+            <DIV class="detallesEntrada">
+                <H3>Nombre Empresa</H3>
+                <HR/>
+                <A href="#" title="Nombre Empresa" class="enlaceEntrada">www.empresa.es</A>
+                <P>
+                    Descripción de la Empresa y trabajo realizado en el sitio web
+                </P>
+            </DIV>
+        </DIV>
+    </DIV>
+    </ARTICLE>
 </SECTION>
 -->
