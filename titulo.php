dffe123837349964fc9e5180a688c10fd1fd3875
Fryntiz <fryntiz@gmail.com>
2017-10-20 12:24:16 +0200

    Reparados espacios/tabulaciones

--- titulo.php
+++ titulo.php
@@ -2,37 +2,37 @@
 <DIV id="binario" onClick="limpiarBinario()"></DIV>
 
 <DIV id="titulo">
-	<A href="http://www.fryntiz.es" title="Web Oficial de Fryntiz">
-		<IMG src="./images/Fryntiz.png" alt="Imagen Fryntiz"/>
-	</A>
-	<A href="http://www.fryntiz.es" title="Web Oficial de Fryntiz">
-		<IMG src="./images/DesarrolladorWEB.png" alt="Imagen Desarrollador WEB"/>
-	</A>
+    <A href="http://www.fryntiz.es" title="Web Oficial de Fryntiz">
+        <IMG src="./images/Fryntiz.png" alt="Imagen Fryntiz"/>
+    </A>
+    <A href="http://www.fryntiz.es" title="Web Oficial de Fryntiz">
+        <IMG src="./images/DesarrolladorWEB.png" alt="Imagen Desarrollador WEB"/>
+    </A>
 </DIV>
 
 <DIV class="cajaMiniLogo">
-	<DIV class="miniLogo">
+    <DIV class="miniLogo">
 
-	</DIV>
+    </DIV>
 </DIV>
 
 
 <DIV id="subtitulo" onClick="cambiarSubtitulo()">
-	<IMG id="IMGsubtitulo" src="./images/Portada.png" alt="Portada WEB Fryntiz"/>
+    <IMG id="IMGsubtitulo" src="./images/Portada.png" alt="Portada WEB Fryntiz"/>
 </DIV>
 
 <HR class="separador" />
 
 <DIV id="presentacion">
-	<SPAN style="color:#688cc7;">WEb oficial de</SPAN> <SPAN style="color:#1d53c7;">Fryntiz</SPAN>
-	<BR/>
-	<SPAN style="color:#be2e00;">www.fryntiz.es</SPAN>
-	<BR/>
-	<SPAN style="color:#688cc7;">Desarrollador</SPAN> <SPAN style="color:#68d162;">WEB</SPAN>
-	<BR/>
-	<SPAN style="color:#f98500;">Técnico</SPAN> <SPAN style="color:#688cc7;">en Sistemas</SPAN>
-	<BR/>
-	<SPAN style="color:#f98500;">Técnico</SPAN> <SPAN style="color:#688cc7;">en Redes</SPAN>
+    <SPAN style="color:#688cc7;">WEb oficial de</SPAN> <SPAN style="color:#1d53c7;">Fryntiz</SPAN>
+    <BR/>
+    <SPAN style="color:#be2e00;">www.fryntiz.es</SPAN>
+    <BR/>
+    <SPAN style="color:#688cc7;">Desarrollador</SPAN> <SPAN style="color:#68d162;">WEB</SPAN>
+    <BR/>
+    <SPAN style="color:#f98500;">Técnico</SPAN> <SPAN style="color:#688cc7;">en Sistemas</SPAN>
+    <BR/>
+    <SPAN style="color:#f98500;">Técnico</SPAN> <SPAN style="color:#688cc7;">en Redes</SPAN>
 </DIV>
 
 <HR class="separador"/>
