76815e6fe87782ea98efa721325ed9a30b27dc27
Raúl Caro Pastorino <tecnico@fryntiz.es>
2017-10-20 18:07:00 +0200

    Eliminadas funciones que ya no se usan

--- JS/scripts.js
+++ JS/scripts.js
@@ -1,24 +1,5 @@
 var ContadorSubtitulo = 0;
 
-function generadorBinario() {
-    var cantidadBucle = 230;
-    while( 1 < cantidadBucle ){
-        var aleatorio = "";
-        if (Math.random() > 0.5) {
-            aleatorio = 1;
-        } else {
-            aleatorio = 0;
-        }
-        document.getElementById("binario").innerHTML = document.getElementById("binario").innerHTML + aleatorio;
-        --cantidadBucle;
-    }
-}
-
-function limpiarBinario() {
-    document.getElementById("binario").innerHTML = "";
-    generadorBinario();
-}
-
 function cambiarPortada() {
     if (ContadorSubtitulo == 0) {
         document.getElementById("IMGsubtitulo").src = "./images/PortadaB.png";
