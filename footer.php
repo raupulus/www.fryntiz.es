910c043240983fee3f5e7a5f372fb54b798fe5a7
Fryntiz <fryntiz@gmail.com>
2017-10-20 12:14:04 +0200

    Corregidos espacios/tabulaciones

--- footer.php
+++ footer.php
@@ -1,17 +1,17 @@
 <DIV id="cajaFooter">
-	<DIV id="barraDecoracion">
+    <DIV id="barraDecoracion">
 
-	</DIV>
+    </DIV>
 
-	<DIV id="centrarFooter">
-		<FOOTER id="footer">
-			<DIV class="seccionFooter"><A href="http://www.fryntiz.es" title="Web Oficial de Fryntiz">www.fryntiz.es</A></DIV>
-			<ADDRESS class="seccionFooter">
-				Fryntiz - <A href="mailto:tecnico@fryntiz.es" title="Correo electrónico de Fryntiz">tecnico@fryntiz.es</A>
-			</ADDRESS>
-			<DIV class="seccionFooter">
-				Raúl Caro Pastorino - 2016
-			</DIV>
-		</FOOTER>
-	</DIV>
+    <DIV id="centrarFooter">
+        <FOOTER id="footer">
+            <DIV class="seccionFooter"><A href="http://www.fryntiz.es" title="Web Oficial de Fryntiz">www.fryntiz.es</A></DIV>
+            <ADDRESS class="seccionFooter">
+                Fryntiz - <A href="mailto:tecnico@fryntiz.es" title="Correo electrónico de Fryntiz">tecnico@fryntiz.es</A>
+            </ADDRESS>
+            <DIV class="seccionFooter">
+                Raúl Caro Pastorino - 2016
+            </DIV>
+        </FOOTER>
+    </DIV>
 </DIV>
