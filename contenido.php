ecbb609191366726651fcbfc2d7ef896e1068bfb
Raúl Caro Pastorino <tecnico@fryntiz.es>
2018-02-11 15:09:28 +0100

    Mejorada sintaxis

--- contenido.php
+++ contenido.php
@@ -3,77 +3,77 @@
          Proyectos
 -------------------------------
 -->
-<SECTION>
-    <ARTICLE>
-    <H2 id="colaboraciones">PROYECTOS DONDE COLABORO</H2>
-    <DIV class="cajaMiniLogo"><DIV class="miniLogo"></DIV></DIV>
-    <DIV class="cajaContenido">
-        <DIV class="cajaEntrada">
-            <DIV class="imagenEntrada">
-                <A href="http://www.laguialinux.es" title="La Guía Linux" target="_blank"><IMG src="./images/laguialinux.png" alt="La Guía Linux" title="La Guía Linux" /></A>
-            </DIV>
+<section>
+    <article>
+    <h2 id="colaboraciones">PROYECTOS DONDE COLABORO</h2>
+    <div class="cajaMiniLogo"><div class="miniLogo"></div></div>
+    <div class="cajaContenido">
+        <div class="cajaEntrada">
+            <div class="imagenEntrada">
+                <a href="http://www.laguialinux.es" title="La Guía Linux" target="_blank"><img src="./images/laguialinux.png" alt="La Guía Linux" title="La Guía Linux" /></a>
+            </div>
 
-            <DIV class="detallesEntrada">
-                <H3>La Guía Linux</H3>
-                <HR/>
-                <A href="http://www.laguialinux.es" title="La Guía Linux" class="enlaceEntrada" target="_blank">www.laguialinux.es</A>
-                <P>
+            <div class="detallesEntrada">
+                <h3>La Guía Linux</h3>
+                <hr />
+                <a href="http://www.laguialinux.es" title="La Guía Linux" class="enlaceEntrada" target="_blank">www.laguialinux.es</a>
+                <p>
                     Sitio WEB con Guías y vídeotutoriales sobre informática, especialmente sobre Linux.
-                </P>
-            </DIV>
-        </DIV>
+                </p>
+            </div>
+        </div>
 
-        <DIV class="cajaEntrada">
-            <DIV class="imagenEntrada">
-                <A href="http://www.desdechipiona.es" title="Desde Chipiona" target="_blank"><IMG src="./images/desdechipiona.png" alt="Desde Chipiona" title="Desde Chipiona" /></A>
-            </DIV>
+        <div class="cajaEntrada">
+            <div class="imagenEntrada">
+                <a href="http://www.desdechipiona.es" title="Desde Chipiona" target="_blank"><img src="./images/desdechipiona.png" alt="Desde Chipiona" title="Desde Chipiona" /></a>
+            </div>
 
-            <DIV class="detallesEntrada">
-                <H3>Desde Chipiona</H3>
-                <HR/>
-                <A href="http://www.desdechipiona.es" title="Desde Chipiona" class="enlaceEntrada" target="_blank">www.desdechipiona.es</A>
-                <P>
+            <div class="detallesEntrada">
+                <h3>Desde Chipiona</h3>
+                <hr />
+                <a href="http://www.desdechipiona.es" title="Desde Chipiona" class="enlaceEntrada" target="_blank">www.desdechipiona.es</a>
+                <p>
                     Proyecto de sitio WEB con información y Galería de imágenes sobre Chipiona
-                </P>
-            </DIV>
-        </DIV>
+                </p>
+            </div>
+        </div>
 
-        <DIV class="cajaEntrada">
-            <DIV class="imagenEntrada">
-                <A href="http://www.nzweb.org" title="NZ Creaciones" target="_blank"><IMG src="./images/nzcreaciones.png" alt="NZ Creaciones" title="NZ Creaciones" /></A>
-            </DIV>
+        <div class="cajaEntrada">
+            <div class="imagenEntrada">
+                <A href="http://www.nzweb.org" title="NZ Creaciones" target="_blank"><img src="./images/nzcreaciones.png" alt="NZ Creaciones" title="NZ Creaciones" /></A>
+            </div>
 
-            <DIV class="detallesEntrada">
-                <H3>NZ Creaciones</H3>
-                <HR/>
-                <A href="http://www.nzcreaciones.es" title="NZ Creaciones" class="enlaceEntrada" target="_blank">www.nzcreaciones.es</A>
-                <P>
+            <div class="detallesEntrada">
+                <h3>NZ Creaciones</h3>
+                <hr />
+                <a href="http://www.nzcreaciones.es" title="NZ Creaciones" class="enlaceEntrada" target="_blank">www.nzcreaciones.es</a>
+                <p>
                     Proyecto que engloba otros proyectos de animación gráfica, entretenimiento, risas y ocio.
-                </P>
-            </DIV>
-        </DIV>
+                </p>
+            </div>
+        </div>
 
-        <DIV class="cajaEntrada">
-            <DIV class="imagenEntrada">
-                <A href="https://lignux.com/" title="Asociación Lignux" target="_blank"><IMG src="./images/asociacionlignux.png" alt="Asociación Lignux" title="Asociación Lignux" /></A>
-            </DIV>
+        <div class="cajaEntrada">
+            <div class="imagenEntrada">
+                <A href="https://lignux.com/" title="Asociación Lignux" target="_blank"><img src="./images/asociacionlignux.png" alt="Asociación Lignux" title="Asociación Lignux" /></A>
+            </div>
 
-            <DIV class="detallesEntrada">
-                <H3>Asociación Lignux</H3>
-                <HR/>
-                <A href="https://lignux.com/" title="Asociación Lignux" class="enlaceEntrada" target="_blank">https://lignux.com/</A>
-                <P>
+            <div class="detallesEntrada">
+                <h3>Asociación Lignux</h3>
+                <hr />
+                <a href="https://lignux.com/" title="Asociación Lignux" class="enlaceEntrada" target="_blank">https://lignux.com/</a>
+                <p>
                     Colaboraciones puntuales creando entradas para este proyecto y ayudando en el mantenimiento.
-                </P>
-            </DIV>
-        </DIV>
-    </DIV>
-    </ARTICLE>
-</SECTION>
+                </p>
+            </div>
+        </div>
+    </div>
+    </article>
+</section>
 
-<BR/><BR/>
-<HR/>
-<BR/>
+<br /><br />
+<hr />
+<br />
 
 <!--
 -------------------------------
@@ -82,28 +82,28 @@
 -->
 
 <!--
-<SECTION>
-    <ARTICLE>
+<section>
+    <article>
     <H2 id="trabajos">SITIOS WEB CREADOS</H2>
-    <DIV class="cajaMiniLogo"><DIV class="miniLogo"></DIV></DIV>
-    <DIV class="cajaContenido">
-        <DIV class="cajaEntrada">
-            <DIV class="imagenEntrada">
-                <A href="#" title="Nombre Empresa" target="_blank"><IMG src="./images/PlantillaImágenes.png" alt="Nombre Empresa" title="Nombre Empresa" /></A>
-            </DIV>
+    <div class="cajaMiniLogo"><div class="miniLogo"></div></div>
+    <div class="cajaContenido">
+        <div class="cajaEntrada">
+            <div class="imagenEntrada">
+                <A href="#" title="Nombre Empresa" target="_blank"><img src="./images/PlantillaImágenes.png" alt="Nombre Empresa" title="Nombre Empresa" /></A>
+            </div>
 
-            <DIV class="detallesEntrada">
+            <div class="detallesEntrada">
                 <H3>Nombre Empresa</H3>
                 <HR/>
                 <A href="#" title="Nombre Empresa" class="enlaceEntrada" target="_blank">www.empresa.es</A>
                 <P>
                     Descripción de la Empresa y trabajo realizado en el sitio web
                 </P>
-            </DIV>
-        </DIV>
-    </DIV>
-    </ARTICLE>
-</SECTION>
+            </div>
+        </div>
+    </div>
+    </article>
+</section>
 
 <BR/><BR/>
 <HR/>
@@ -117,85 +117,85 @@
 -------------------------------
 -->
 
-<SECTION>
-    <ARTICLE>
-    <H2>OTROS PROYECTOS</H2>
-    <DIV class="cajaMiniLogo"><DIV class="miniLogo"></DIV></DIV>
-    <DIV class="cajaContenido">
-        <DIV class="cajaEntrada">
-            <DIV class="imagenEntrada">
-                <A href="https://github.com/fryntiz/bot_twitter" title="Bot Twitter" target="_blank"><IMG src="./images/bot_twitter.png" alt="Bot Twitter" title="Nombre Empresa" /></A>
-            </DIV>
+<section>
+    <article>
+    <h2>OTROS PROYECTOS</h2>
+    <div class="cajaMiniLogo"><div class="miniLogo"></div></div>
+    <div class="cajaContenido">
+        <div class="cajaEntrada">
+            <div class="imagenEntrada">
+                <a href="https://github.com/fryntiz/bot_twitter" title="Bot Twitter" target="_blank"><img src="./images/bot_twitter.png" alt="Bot Twitter" title="Nombre Empresa" /></a>
+            </div>
 
-            <DIV class="detallesEntrada">
-                <H3>Bot Twitter</H3>
-                <HR/>
-                <A href="https://github.com/fryntiz/bot_twitter" title="Bot Twitter" class="enlaceEntrada" target="_blank">https://github.com</A>
-                <P>
+            <div class="detallesEntrada">
+                <h3>Bot Twitter</h3>
+                <hr />
+                <a href="https://github.com/fryntiz/bot_twitter" title="Bot Twitter" class="enlaceEntrada" target="_blank">https://github.com</a>
+                <p>
                     Bot para interactuar en las redes sociales de forma independiente para publicar, retwittear, fav...
-                </P>
-            </DIV>
-        </DIV>
+                </p>
+            </div>
+        </div>
 
-        <DIV class="cajaEntrada">
-            <DIV class="imagenEntrada">
-                <A href="https://github.com/fryntiz/preparar_entorno" title="Script Debian" target="_blank"><IMG src="./images/script_debian.png" alt="Script Debian" title="Script Debian" /></A>
-            </DIV>
+        <div class="cajaEntrada">
+            <div class="imagenEntrada">
+                <a href="https://github.com/fryntiz/preparar_entorno" title="Script Debian" target="_blank"><img src="./images/script_debian.png" alt="Script Debian" title="Script Debian" /></a>
+            </div>
 
-            <DIV class="detallesEntrada">
-                <H3>Script Debian</H3>
-                <HR/>
-                <A href="https://github.com/fryntiz/preparar_entorno" title="Script Debian" class="enlaceEntrada" target="_blank">https://github.com</A>
-                <P>
+            <div class="detallesEntrada">
+                <h3>Script Debian</h3>
+                <hr />
+                <a href="https://github.com/fryntiz/preparar_entorno" title="Script Debian" class="enlaceEntrada" target="_blank">https://github.com</a>
+                <p>
                     Conjuntos de scripts para adaptar un sistema operativo Debian a mi gusto para desarrollo web
-                </P>
-            </DIV>
-        </DIV>
+                </p>
+            </div>
+        </div>
 
-        <DIV class="cajaEntrada">
-            <DIV class="imagenEntrada">
-                <A href="https://github.com/fryntiz/fondos_debian" title="Diseño en Debian" target="_blank"><IMG src="./images/disenio_debian.png" alt="Diseño en Debian" title="Diseño en Debian" /></A>
-            </DIV>
+        <div class="cajaEntrada">
+            <div class="imagenEntrada">
+                <A href="https://github.com/fryntiz/fondos_debian" title="Diseño en Debian" target="_blank"><img src="./images/disenio_debian.png" alt="Diseño en Debian" title="Diseño en Debian" /></A>
+            </div>
 
-            <DIV class="detallesEntrada">
-                <H3>Diseño en Debian</H3>
-                <HR/>
-                <A href="https://github.com/fryntiz/fondos_debian" title="Diseño en Debian" class="enlaceEntrada" target="_blank">https://github.com</A>
-                <P>
+            <div class="detallesEntrada">
+                <h3>Diseño en Debian</h3>
+                <hr />
+                <a href="https://github.com/fryntiz/fondos_debian" title="Diseño en Debian" class="enlaceEntrada" target="_blank">https://github.com</a>
+                <p>
                     Mis diseños de fondos para el Escritorio, GRUB, Playmouth, GDM... sobre Debian
-                </P>
-            </DIV>
-        </DIV>
+                </p>
+            </div>
+        </div>
 
-        <DIV class="cajaEntrada">
-            <DIV class="imagenEntrada">
-                <A href="https://github.com/fryntiz/Mini-Aplicaciones-Web" title="Mini Apps WEB" target="_blank"><IMG src="./images/miniapps.png" alt="Mini Apps WEB" title="Mini Apps WEB" /></A>
-            </DIV>
+        <div class="cajaEntrada">
+            <div class="imagenEntrada">
+                <a href="https://github.com/fryntiz/Mini-Aplicaciones-Web" title="Mini Apps WEB" target="_blank"><img src="./images/miniapps.png" alt="Mini Apps WEB" title="Mini Apps WEB" /></a>
+            </div>
 
-            <DIV class="detallesEntrada">
-                <H3>Mini Apps WEB</H3>
-                <HR/>
-                <A href="https://github.com/fryntiz/Mini-Aplicaciones-Web" title="Mini Apps WEB" class="enlaceEntrada" target="_blank">https://github.com</A>
-                <P>
+            <div class="detallesEntrada">
+                <h3>Mini Apps WEB</h3>
+                <hr />
+                <a href="https://github.com/fryntiz/Mini-Aplicaciones-Web" title="Mini Apps WEB" class="enlaceEntrada" target="_blank">https://github.com</a>
+                <p>
                     Pequeñas aplicaciones web que he realizado para integrarlas en otros proyectos
-                </P>
-            </DIV>
-        </DIV>
+                </p>
+            </div>
+        </div>
 
-        <DIV class="cajaEntrada">
-            <DIV class="imagenEntrada">
-                <A href="https://github.com/fryntiz/SuperScriptBash" title="SuperBashScript" target="_blank"><IMG src="./images/superbashscript.png" alt="SuperBashScript" title="SuperBashScript" /></A>
-            </DIV>
+        <div class="cajaEntrada">
+            <div class="imagenEntrada">
+                <a href="https://github.com/fryntiz/SuperScriptBash" title="SuperBashScript" target="_blank"><img src="./images/superbashscript.png" alt="SuperBashScript" title="SuperBashScript" /></a>
+            </div>
 
-            <DIV class="detallesEntrada">
-                <H3>SuperBashScript</H3>
-                <HR/>
-                <A href="https://github.com/fryntiz/SuperScriptBash" title="SuperBashScript" class="enlaceEntrada" target="_blank">https://github.com</A>
-                <P>
+            <div class="detallesEntrada">
+                <h3>SuperBashScript</h3>
+                <hr />
+                <a href="https://github.com/fryntiz/SuperScriptBash" title="SuperBashScript" class="enlaceEntrada" target="_blank">https://github.com</a>
+                <p>
                     Script para ayudar con administración, mantenimiento y acceso a programas Shell Linux.
-                </P>
-            </DIV>
-        </DIV>
-    </DIV>
-    </ARTICLE>
-</SECTION>
+                </p>
+            </div>
+        </div>
+    </div>
+    </article>
+</section>
