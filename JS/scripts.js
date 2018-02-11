501bb5a7724b3f99f9974579db21ed3ae523209a
Raúl Caro Pastorino <tecnico@fryntiz.es>
2018-02-11 14:34:00 +0100

    Refactorizado archivo de scripts

--- JS/scripts.js
+++ JS/scripts.js
@@ -1,11 +1,15 @@
 var ContadorSubtitulo = 0;
 
+/**
+ * Función para cambiar la imagen de la portada
+ * @returns {number} Devuelve el número de la portada actual
+ */
 function cambiarPortada() {
-    if (ContadorSubtitulo == 0) {
-        document.getElementById("IMGsubtitulo").src = "./images/PortadaB.png";
+    if (ContadorSubtitulo === 0) {
+        document.getElementById('IMGsubtitulo').src = './images/PortadaB.png';
         return ++ContadorSubtitulo;
-    }else if (ContadorSubtitulo == 1) {
-        document.getElementById("IMGsubtitulo").src = "./images/Portada.png";
+    } else if (ContadorSubtitulo === 1) {
+        document.getElementById('IMGsubtitulo').src = './images/Portada.png';
         return --ContadorSubtitulo;
     }
 }
