db32bc58949fe09b28f27af44b6bfde5fcc26d30
Raúl Caro Pastorino <tecnico@fryntiz.es>
2018-02-11 15:13:06 +0100

    Mejorada sintaxis

--- footer.php
+++ footer.php
@@ -1,17 +1,29 @@
-<DIV id="cajaFooter">
-    <DIV id="barraDecoracion">
+<div id="cajaFooter">
+    <div id="barraDecoracion">
 
-    </DIV>
+    </div>
 
-    <DIV id="centrarFooter">
-        <FOOTER id="footer">
-            <DIV class="seccionFooter"><A href="https://github.com/fryntiz" title="Repositorios para el desarrollo de Raúl Caro Pastorino, Fryntiz">https://github.com/fryntiz</A></DIV>
-            <ADDRESS class="seccionFooter">
-                Correo → <A href="mailto:tecnico@fryntiz.es" title="Correo electrónico de Fryntiz">tecnico@fryntiz.es</A>
-            </ADDRESS>
-            <DIV class="seccionFooter">
-                (<?php echo date(Y); ?>) Sitio web bajo GPLv3
-            </DIV>
-        </FOOTER>
-    </DIV>
-</DIV>
+    <div id="centrarFooter">
+        <footer id="footer">
+            <div class="seccionFooter">
+                <a href="https://github.com/fryntiz"
+                   title="Repositorios para el
+                          desarrollo de Raúl Caro Pastorino,
+                          Fryntiz">
+                    https://github.com/fryntiz
+                </a>
+            </div>
+
+            <address class="seccionFooter">
+                Correo → <a href="mailto:tecnico@fryntiz.es"
+                            title="Correo electrónico de Fryntiz">
+                    tecnico@fryntiz.es
+                </a>
+            </address>
+
+            <div class="seccionFooter">
+                (<?php echo date('Y'); ?>) Sitio web bajo GPLv3
+            </div>
+        </footer>
+    </div>
+</div>
