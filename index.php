d67f03f5b5de7afe467f54335d36197cffaa0daa
Raúl Caro Pastorino <tecnico@fryntiz.es>
2018-02-11 14:34:15 +0100

    Refactorizado archivo principal

--- index.php
+++ index.php
@@ -1,46 +1,53 @@
 <!DOCTYPE HTML>
-<HTML lang="es">
-    <HEAD>
-        <META charset="utf-8"/>
-        <TITLE>Fryntiz Desarrollador WEB</TITLE>
-        <META name="description" content="Fryntiz Desarrollador de aplicaciones WEB, página oficial"/>
-        <META name="keywords" content="curriculum, fryntiz, programador, desarrollador, página web, web, online, curriculum online, desarrollador web, sitio web, raul, raul caro pastorino, Raúl Caro Pastorino, Raúl, Raúl Caro, Caro Pastorino, web developer, developer, crear página, crear página web, hacer página web"/>
-        <META name="author" content="Raúl Caro Pastorino"/>
-<!--Metaetiquetas para Facebook-->
-        <META property="og:title" content="Fryntiz Desarrollador WEB"/>
-        <META property="og:description" content="Sitio WEB Oficial de Raúl Caro Pastorino (Fryntiz)" />
-        <META property="og:image" content="./images/LogoFryntiz.png"/>
-<!--Metaetiquetas para Twitter-->
-        <META name="twitter:title" content="Fryntiz Desarrollador WEB"/>
-        <META name="twitter:description" content="Sitio WEB Oficial de Raúl Caro Pastorino (Fryntiz)" />
-        <META name="twitter:image:src" content="./images/LogoFryntiz.png"/>
-        <META name="twitter:site" content="@fryntiz"/>
-        <META name="twitter:creator" content="@fryntiz"/>
-<!--Metaetiquetas para Google+-->
-        <LINK href="https://plus.google.com/109655402661721084499/posts" rel="author"/>
-        <LINK href="https://plus.google.com/109655402661721084499" rel="publisher"/>
-        <META itemprop="name" content="Fryntiz Desarrollador WEB"/>
-        <META itemprop="description" content="Sitio WEB Oficial de Raúl Caro Pastorino (Fryntiz)" />
-        <META itemprop="image" content="./images/LogoFryntiz.png"/>
+<html lang="es">
+    <head>
+        <meta charset="utf-8" />
+        <title>Fryntiz Desarrollador WEB</title>
+        <meta name="description" content="Fryntiz Desarrollador de
+                                          aplicaciones WEB, página oficial" />
+        <meta name="keywords" content="curriculum, fryntiz, programador,
+                                       desarrollador, página web, web,
+                                       online, curriculum online,
+                                       desarrollador web, sitio web, raul,
+                                       raul caro pastorino, Raúl Caro
+                                       Pastorino, Raúl, Raúl Caro, Caro
+                                       Pastorino, web developer, developer,
+                                       crear página, crear página web, hacer
+                                       página web" />
+        <meta name="author" content="Raúl Caro Pastorino" />
+<!--metaetiquetas para Facebook-->
+        <meta property="og:title" content="Fryntiz Desarrollador WEB" />
+        <meta property="og:description" content="Sitio WEB Oficial de Raúl Caro Pastorino (Fryntiz)" />
+        <meta property="og:image" content="./images/LogoFryntiz.png" />
+<!--metaetiquetas para Twitter-->
+        <meta name="twitter:title" content="Fryntiz Desarrollador WEB" />
+        <meta name="twitter:description" content="Sitio WEB Oficial de Raúl Caro Pastorino (Fryntiz)" />
+        <meta name="twitter:image:src" content="./images/LogoFryntiz.png" />
+        <meta name="twitter:site" content="@fryntiz" />
+        <meta name="twitter:creator" content="@fryntiz" />
+<!--metaetiquetas para Google+-->
+        <link href="https://plus.google.com/109655402661721084499/posts" rel="author"/>
+        <link href="https://plus.google.com/109655402661721084499" rel="publisher"/>
+        <meta itemprop="name" content="Fryntiz Desarrollador WEB" />
+        <meta itemprop="description" content="Sitio WEB Oficial de Raúl Caro Pastorino (Fryntiz)" />
+        <meta itemprop="image" content="./images/LogoFryntiz.png" />
 <!--Enlaces de archivos que componen el sitio-->
-        <LINK rel="shortcut icon" href="./images/favicon.png"/>
-        <LINK rel="stylesheet" href="./CSS/styles.css"/>
-        <LINK rel="stylesheet" href="./CSS/titulo.css"/>
-        <LINK rel="stylesheet" href="./CSS/contenido.css"/>
-        <LINK rel="stylesheet" href="./CSS/footer.css"/>
-        <SCRIPT src="JS/scripts.js"></SCRIPT>
-    </HEAD>
+        <link rel="shortcut icon" href="./images/favicon.png" />
+        <link rel="stylesheet" href="./CSS/styles.css" />
+        <link rel="stylesheet" href="./CSS/titulo.css" />
+        <link rel="stylesheet" href="./CSS/contenido.css" />
+        <link rel="stylesheet" href="./CSS/footer.css" />
+        <script src="JS/scripts.js"></script>
+    </head>
 
-    <BODY OnLoad="generadorBinario()">
-        <DIV>
+    <body>
+        <div>
             <?php
-                require "titulo.php";
-                require "contenido.php";
+                require 'titulo.php';
+                require 'contenido.php';
             ?>
-        </DIV>
+        </div>
 
-            <?php
-                require "footer.php";
-            ?>
-    </BODY>
-</HTML>
+            <?php require 'footer.php'; ?>
+    </body>
+</html>
