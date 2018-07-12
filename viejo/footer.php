e441d279e5de3a61f1ef2936968391492eaf1add
Raúl Caro Pastorino <tecnico@fryntiz.es>
2018-07-13 00:10:59 +0200

    Convirtiendo proyecto en angular

---
+++ viejo/footer.php
@@ -0,0 +1,29 @@
+<div id="cajaFooter">
+    <div id="barraDecoracion">
+
+    </div>
+
+    <div id="centrarFooter">
+        <footer id="footer">
+            <div class="seccionFooter">
+                <a href="https://github.com/fryntiz"
+                   title="Repositorios para el
+                          desarrollo de Raúl Caro Pastorino,
+                          Fryntiz">
+                    https://github.com/fryntiz
+                </a>
+            </div>
+
+            <address class="seccionFooter">
+                Correo → <a href="mailto:tecnico@fryntiz.es"
+                            title="Correo electrónico de Fryntiz">
+                    tecnico@fryntiz.es
+                </a>
+            </address>
+
+            <div class="seccionFooter">
+                (<?php echo date('Y'); ?>) Sitio web bajo GPLv3
+            </div>
+        </footer>
+    </div>
+</div>
---
+++ viejo/index.php
@@ -0,0 +1,59 @@
+<!DOCTYPE HTML>
+<html lang="es">
+    <head>
+        <meta charset="utf-8" />
+        <title>Fryntiz Desarrollador WEB</title>
+        <meta name="description" content="Fryntiz Desarrollador de
+                                          aplicaciones WEB, página oficial" />
+        <meta name="keywords" content="curriculum, fryntiz, programador,
+                                       desarrollador, página web, web,
+                                       online, curriculum online,
+                                       desarrollador web, sitio web, raul,
+                                       raul caro pastorino, Raúl Caro
+                                       Pastorino, Raúl, Raúl Caro, Caro
+                                       Pastorino, web developer, developer,
+                                       crear página, crear página web, hacer
+                                       página web" />
+        <meta name="author" content="Raúl Caro Pastorino" />
+<!--metaetiquetas para Facebook-->
+        <meta property="og:title" content="Fryntiz Desarrollador WEB" />
+        <meta property="og:description" content="Sitio WEB Oficial de Raúl
+                                                 Caro Pastorino (Fryntiz)" />
+        <meta property="og:image" content="./images/LogoFryntiz.png" />
+<!--metaetiquetas para Twitter-->
+        <meta name="twitter:title" content="Fryntiz Desarrollador WEB" />
+        <meta name="twitter:description" content="Sitio WEB Oficial de Raúl
+                                                  Caro Pastorino (Fryntiz)" />
+        <meta name="twitter:image:src" content="./images/LogoFryntiz.png" />
+        <meta name="twitter:site" content="@fryntiz" />
+        <meta name="twitter:creator" content="@fryntiz" />
+<!--metaetiquetas para Google+-->
+        <link href="https://plus.google.com/109655402661721084499/posts"
+              rel="author" />
+        <link href="https://plus.google.com/109655402661721084499"
+              rel="publisher"/>
+        <meta itemprop="name" content="Fryntiz Desarrollador WEB" />
+        <meta itemprop="description" content="Sitio WEB Oficial de Raúl Caro
+                                              Pastorino (Fryntiz)" />
+        <meta itemprop="image" content="./images/LogoFryntiz.png" />
+<!--Enlaces de archivos que componen el sitio-->
+        <link rel="shortcut icon" href="./images/favicon.png" />
+        <link rel="stylesheet" href="./CSS/styles.css" />
+        <link rel="stylesheet" href="./CSS/titulo.css" />
+        <link rel="stylesheet" href="./CSS/contenido.css" />
+        <link rel="stylesheet" href="./CSS/footer.css" />
+        <script src="JS/jquery-3.3.1.js"></script>
+        <script src="JS/scripts.js"></script>
+    </head>
+
+    <body>
+        <div>
+            <?php
+                require 'titulo.php';
+                require 'contenido.php';
+            ?>
+        </div>
+
+            <?php require 'footer.php'; ?>
+    </body>
+</html>
