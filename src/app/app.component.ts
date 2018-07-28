2db4f8fb9b1708799be2492d8af1e896fbf6b49e
Raúl Caro Pastorino <tecnico@fryntiz.es>
2018-07-28 16:54:14 +0200

    Modifico componente de aplicación principal

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: 'app-root',  // Nombre de la etiqueta html personalizada
+  templateUrl: './app.component.html', // Vista HTML para el componente
+  styleUrls: ['./app.component.css']   // Estilos CSS para el componente 
 })
+
+// Exporto la clase con el nombre que declaro en el APP MODULE
 export class AppComponent {
   title = 'app';
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,47 @@
+// Importo clase BrowserModule encargada de buscar módulos de angular.
 import { BrowserModule } from '@angular/platform-browser';
+
+// Importo clase NgModule la cual es el módulo raíz.
 import { NgModule } from '@angular/core';
 
+// Agrego HttpModule para trabajar con cabeceras HTTP
+import { HttpModule } from '@angular/http';
+
+// Agrego FormsModule para trabajar con formulario
+import { FormsModule } from '@angular/forms';
+
+// Importo cada componente propio para la aplicación
 import { AppComponent } from './app.component';
+import { IndexComponent } from './index/index.component';
+import { ContactComponent } from './contact/contact.component';
+import { BarComponent } from './bar/bar.component';
+
+// Importo módulos de rutas
+import { routing, appRoutingProviders } from './rutas';
 
 @NgModule({
+  // Clases de vista para el módulo (components|directives|pipes)
   declarations: [
-    AppComponent
+    AppComponent,
+    IndexComponent,
+    ContactComponent,
+    BarComponent
   ],
+
+  // Importo módulos que usarán las plantillas
   imports: [
-    BrowserModule
+    BrowserModule,
+    routing,     // Módulo de rutas
+    HttpModule,  // Módulo HTTP
+    FormsModule, // Módulo de formularios
   ],
-  providers: [],
+
+  // Los proveedores crean servicios de este módulo globalmente a la aplicación
+  providers: [
+    appRoutingProviders,  // Cargo servicio para usar las rutas
+  ],
   bootstrap: [AppComponent]
 })
+
+// Se exporta la clase del módulo raíz y así puede ser invocada desde otro lugar
 export class AppModule { }
