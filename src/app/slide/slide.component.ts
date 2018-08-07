88826b5bec170bb04518eaa0f7319b1a6e1dafa4
Raúl Caro Pastorino <tecnico@fryntiz.es>
2018-08-07 23:10:39 +0200

    Componente para slide ahora toma los datos que provienen de JSON

--- src/app/slide/slide.component.ts
+++ src/app/slide/slide.component.ts
@@ -1,48 +1,35 @@
 import { Component, OnInit } from '@angular/core';
 
+// Importo Servicio para obtener información desde JSON
+import { ServiceData } from '../../services/data.service';
+
 @Component({
   selector: 'app-slide',
   templateUrl: './slide.component.html',
-  styleUrls: ['./slide.component.css']
+  styleUrls: [ './slide.component.css' ],
+  providers: [ ServiceData ]
 })
+
 export class SlideComponent implements OnInit {
+  private config;
+  private slideConfig;
+  private imageUrls;
 
-    imageUrls = [
-      { url: '/assets/img/slide/slide1.jpg', caption: 'Primer slide', href: '#', clickAction: () => console.log('Esto es un evento de img 1') },
-      { url: '/assets/img/slide/slide2.jpg', caption: 'Segundo slide', href: '#', clickAction: () => console.log('Esto es un evento de img 2') },
-      { url: '/assets/img/slide/slide3.jpg', caption: 'Tercer slide', href: '#', clickAction: () => console.log('Esto es un evento de img 3') },
-      { url: '/assets/img/slide/slide4.jpg', caption: 'Cuarto slide', href: '#', clickAction: () => console.log('Esto es un evento de img 4') },
-    ];
-    height: string = '400px';
-    minHeight: string;
-    arrowSize: string = '30px';
-    showArrows: boolean = true;
-    disableSwiping: boolean = false;
-    autoPlay: boolean = true;
-    autoPlayInterval: number = 3333;
-    stopAutoPlayOnSlide: boolean = true;
-    debug: boolean = true;
-    backgroundSize: string = 'cover';
-    backgroundPosition: string = 'center center';
-    backgroundRepeat: string = 'no-repeat';
-    showDots: boolean = true;
-    dotColor: string = '#FFF';
-    showCaptions: boolean = true;
-    captionColor: string = '#FFF';
-    captionBackground: string = 'rgba(0, 0, 0, .35)';
-    lazyLoad: boolean = false;
-  width: string = '100%';
+  /*
+    Información sobre el componente slide:
+    https://www.npmjs.com/package/ng-simple-slideshow
+  */
 
-  constructor() { }
+  constructor( private _ServiceData:ServiceData ) {
+    // Obtengo los datos de Configuración
+    this.config = _ServiceData.getConfig();
 
-  ngOnInit() {
-      // Agregando imágenes dinámicamente
-    /*
-    setTimeout(() => {
-      console.log('adding an image url dynamically.');
-      this.imageUrls.push('https://cdn-images-1.medium.com/max/2000/1*Nccd2ofdArlXF7v58UK94Q.jpeg');
-    }, 5000);
-    */
+    // Obtengo la configuración para el slide
+    this.slideConfig = _ServiceData.getSlide().config
+
+    // Obtengo los datos directamente para el slide
+    this.imageUrls = _ServiceData.getSlide().img;
   }
 
+  ngOnInit() {}
 }
