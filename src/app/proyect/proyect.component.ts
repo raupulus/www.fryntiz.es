2495ed563658c52a52dbe99f776338f12fe46628
Raúl Caro Pastorino <tecnico@fryntiz.es>
2018-08-08 08:37:19 +0200

    Adapto lógica para funcionar con datos importados desde el servicio

--- src/app/proyect/proyect.component.ts
+++ src/app/proyect/proyect.component.ts
@@ -1,15 +1,27 @@
 import { Component, OnInit } from '@angular/core';
 
+// Importo Servicio para obtener información desde JSON
+import { ServiceData } from '../../services/data.service';
+
 @Component({
   selector: 'app-proyect',
   templateUrl: './proyect.component.html',
-  styleUrls: ['./proyect.component.css']
+  styleUrls: ['./proyect.component.css'],
+  providers: [ServiceData]
 })
 export class ProyectComponent implements OnInit {
+  private datos;
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(private _ServiceData:ServiceData) {
+    // Obtengo los datos directamente para los proyectos
+    this.datos = _ServiceData.getProyect();
   }
 
+  ngOnInit() {
+
+  }
+
+  linkTo(url:string) {
+    window.open(url, '_blank');
+  }
 }
