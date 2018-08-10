fd575637e5bd934b9f4d0e9a1cdbff5b4182aecc
Raúl Caro Pastorino <tecnico@fryntiz.es>
2018-08-10 17:11:10 +0200

    Obtengo datos de forma dinámica para trabajos

--- src/app/jobs/jobs.component.ts
+++ src/app/jobs/jobs.component.ts
@@ -1,15 +1,26 @@
 import { Component, OnInit } from '@angular/core';
 
+// Importo Servicio para obtener información desde JSON
+import { ServiceData } from '../../services/data.service';
+
 @Component({
   selector: 'app-jobs',
   templateUrl: './jobs.component.html',
-  styleUrls: ['./jobs.component.css']
+  styleUrls: ['./jobs.component.css'],
+  providers: [ServiceData]
 })
 export class JobsComponent implements OnInit {
+  private datos:ServiceData;
 
-  constructor() { }
+  constructor(private _ServiceData:ServiceData) {
+    // Obtengo los datos directamente para el menú
+    this.datos = _ServiceData.getJobs();
+  }
 
   ngOnInit() {
   }
 
+  linkTo(url:string) {
+    window.open(url, '_blank');
+  }
 }
