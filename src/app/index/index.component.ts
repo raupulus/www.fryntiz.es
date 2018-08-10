852f98b184d6bfedab81cd57576ed9a4298a3295
Raúl Caro Pastorino <tecnico@fryntiz.es>
2018-08-10 19:30:23 +0200

    Implemento función para desordenar array y devolverlo

--- src/app/index/index.component.ts
+++ src/app/index/index.component.ts
@@ -6,9 +6,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./index.component.css']
 })
 export class IndexComponent implements OnInit {
+  private tags = ['Html5', 'Javascript', 'CSS', 'Php', 'Ajax', 'Shell Script',
+                  'Python', 'jQuery', 'MySQL', 'Sqlite', 'PostgreSQL', 'JSON',
+                  'Xml', 'Angular',
+    ];
 
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit() {
+    // Mezclar contenido del array
+    this.tags = this.tags.sort(function() {
+      return Math.random() - 0.5
+    });
+  }
+
+  getTags() {
+    return this.tags;
   }
 }
