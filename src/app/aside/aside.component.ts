769f02221ebb18dcc0543d1a82f454c7612ec4e1
Raúl Caro Pastorino <tecnico@fryntiz.es>
2018-08-10 13:32:32 +0200

    Agrego servicios

--- src/app/aside/aside.component.ts
+++ src/app/aside/aside.component.ts
@@ -1,13 +1,26 @@
 import { Component, OnInit } from '@angular/core';
 
+// Importo Servicio para obtener información desde JSON
+import { ServiceData } from '../../services/data.service';
+
 @Component({
   selector: 'app-aside',
   templateUrl: './aside.component.html',
-  styleUrls: ['./aside.component.css']
+  styleUrls: ['./aside.component.css'],
+  providers: [ServiceData]
 })
-export class AsideComponent implements OnInit {
 
-  constructor() { }
+export class AsideComponent implements OnInit {
+  private datos:ServiceData;
+  private contact:string;
+
+  constructor(private _ServiceData:ServiceData) {
+    // Obtengo nombre de los menús
+    this.contact = _ServiceData.getMenubar().contact;
+    
+    // Obtengo los datos directamente para el menú
+    this.datos = _ServiceData.getAside();
+  }
 
   ngOnInit() {
   }
