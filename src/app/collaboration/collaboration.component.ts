58d5a5ad7733c92aa535504ff687acdc38615661
Raúl Caro Pastorino <tecnico@fryntiz.es>
2018-08-10 11:24:58 +0200

    Adapto componente para recibir datos

--- src/app/collaboration/collaboration.component.ts
+++ src/app/collaboration/collaboration.component.ts
@@ -1,15 +1,31 @@
 import { Component, OnInit } from '@angular/core';
 
+// Importo Servicio para obtener información desde JSON
+import { ServiceData } from '../../services/data.service';
+
 @Component({
   selector: 'app-collaboration',
   templateUrl: './collaboration.component.html',
-  styleUrls: ['./collaboration.component.css']
+  styleUrls: ['./collaboration.component.css'],
+  providers: [ServiceData]
 })
 export class CollaborationComponent implements OnInit {
+  private config:ServiceData;
+  private datos:ServiceData;
 
-  constructor() { }
+  constructor(private _ServiceData:ServiceData) {
+    // Obtengo los datos de Configuración
+    this.config = _ServiceData.getConfig();
+
+    // Obtengo los datos directamente para el menú
+    this.datos = _ServiceData.getCollaboration();
+  }
 
   ngOnInit() {
+  }
+
+  linkTo(url:string) {
+    window.open(url, '_blank');
   }
 
 }
