8680cb98467019dc30726b718a7887e59f754f72
Raúl Caro Pastorino <tecnico@fryntiz.es>
2018-08-13 19:58:57 +0200

    Creo estilos mínimos para el footer

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -1,15 +1,26 @@
 import { Component, OnInit } from '@angular/core';
 
+// Importo Servicio para obtener información desde JSON
+import { ServiceData } from '../../services/data.service';
+
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
-  styleUrls: ['./footer.component.css']
+  styleUrls: [ './footer.component.css' ],
+  providers: [ ServiceData ]
 })
 export class FooterComponent implements OnInit {
+  private config;
+  private contact;
 
-  constructor() { }
+  constructor( private _ServiceData:ServiceData ) {
+    // Obtengo configuraciones de la aplicación
+    this.config = _ServiceData.getConfig();
+
+    // Obtengo nombre de contacto
+    this.contact = _ServiceData.getMenubar().contact;
+  }
 
   ngOnInit() {
   }
-
 }
