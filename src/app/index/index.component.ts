5d65835280f7d07ea017ff393d6599accdcdd8f5
Raúl Caro Pastorino <tecnico@fryntiz.es>
2018-08-14 09:54:13 +0200

    Añado proveedor de servicios desde json

--- src/app/index/index.component.ts
+++ src/app/index/index.component.ts
@@ -1,17 +1,24 @@
 import { Component, OnInit } from '@angular/core';
 
+// Importo Servicio para obtener información desde JSON
+import { ServiceData } from '../../services/data.service';
+
 @Component({
   selector: 'app-index',
   templateUrl: './index.component.html',
-  styleUrls: ['./index.component.css']
+  styleUrls: ['./index.component.css'],
+  providers: [ServiceData]
 })
 export class IndexComponent implements OnInit {
+  private datos:ServiceData;
   private tags = [
     'Html5', 'Javascript', 'CSS', 'Php', 'Ajax', 'Shell Script', 'Python',
     'jQuery', 'MySQL', 'Sqlite', 'PostgreSQL', 'JSON', 'Xml', 'Angular',
   ];
 
-  constructor() {
+  constructor(private _ServiceData:ServiceData) {
+    // Obtengo los datos directamente para el menú
+    this.datos = _ServiceData.getHobbies();
   }
 
   ngOnInit() {
@@ -23,5 +30,9 @@ export class IndexComponent implements OnInit {
 
   getTags() {
     return this.tags;
+  }
+
+  public scrollTo(anchor) {
+    document.getElementById(anchor).scrollIntoView();
   }
 }
