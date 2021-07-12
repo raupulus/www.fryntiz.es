5562226fb50a79bb8c40efa2fdab1cb37358e50d
Raúl Caro Pastorino <dev@fryntiz.es>
2021-07-12 20:58:47 +0200

    App import router and tailwind

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,11 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import router from "./router";
+//import store from "./store";
 
-createApp(App).mount('#app')
+import "@/assets/css/tailwind.css";
+
+//.use(store)
+createApp(App).use(router)
+  .use(router)
+  .mount("#app");
