709de335395f0c7023e80f42fd598300bf652785
Raúl Caro Pastorino <dev@fryntiz.es>
2021-07-13 21:01:56 +0200

    Added stack block with images at home

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -79,6 +79,79 @@
         </div>
       </div>
     </section>
+
+    <!-- Tecnologías -->
+    <section class="bg-white py-20">
+      <div class="container px-8 mx-auto sm:px-12 lg:px-20">
+        <!--
+        <h1
+          class="text-sm font-bold tracking-wide text-center text-gray-800 uppercase mb-7"
+        >
+          Tecnologías Preferidas
+        </h1>
+        -->
+        <div
+          class="flex grid items-center justify-center grid-cols-4 grid-cols-12 gap-y-8"
+        >
+          <div
+            class="flex items-center justify-center col-span-6 sm:col-span-4 md:col-span-3 xl:col-span-2"
+          >
+            <img
+              src="@/assets/images/stack/php.svg"
+              alt="PHP"
+              class="block object-contain h-12"
+            />
+          </div>
+          <div
+            class="flex items-center justify-center col-span-6 sm:col-span-4 md:col-span-3 xl:col-span-2"
+          >
+            <img
+              src="@/assets/images/stack/laravel.svg"
+              alt="Laravel"
+              class="block object-contain h-9"
+            />
+          </div>
+          <div
+            class="flex items-center justify-center col-span-6 sm:col-span-4 md:col-span-3 xl:col-span-2"
+          >
+            <img
+              src="@/assets/images/stack/javascript.svg"
+              alt="Javascript"
+              class="block object-contain h-9"
+            />
+          </div>
+          <div
+            class="flex items-center justify-center col-span-6 sm:col-span-4 md:col-span-3 xl:col-span-2"
+          >
+            <img
+              src="@/assets/images/stack/vue.svg"
+              alt="Vue"
+              class="block object-contain h-7 lg:h-8"
+            />
+          </div>
+
+          <div
+            class="flex items-center justify-center col-span-6 sm:col-span-4 md:col-span-6 xl:col-span-2"
+          >
+            <img
+              src="@/assets/images/stack/tailwindcss.svg"
+              alt="Tailwind CSS"
+              class="block object-contain h-9"
+            />
+          </div>
+
+          <div
+            class="flex items-center justify-center col-span-6 sm:col-span-4 md:col-span-6 xl:col-span-2"
+          >
+            <img
+              src="@/assets/images/stack/postgresql.svg"
+              alt="PostgreSQL"
+              class="block object-contain h-9"
+            />
+          </div>
+        </div>
+      </div>
+    </section>
   </div>
 </template>
 
