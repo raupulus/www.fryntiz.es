53b8b7fee8456e1f612d5ab69b8566f72c120328
Raúl Caro Pastorino <dev@fryntiz.es>
2021-07-20 00:26:27 +0200

    Created base components for project list

---
+++ src/components/projects/ProjectCard.vue
@@ -0,0 +1,36 @@
+<template>
+  <section></section>
+</template>
+
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "ProjectCard",
+
+  setup() {
+    /*
+        function onSwiper(swiper) {
+            console.log(swiper);
+        }
+
+        function onSlideChange() {
+            console.log('slide change');
+        };
+        return {
+            onSwiper,
+            onSlideChange
+        }
+        */
+  },
+  /*
+    methods: {
+      
+    },
+    */
+});
+</script>
+
+<style scoped lang="scss">
+</style>
