07c31ab8f29c147e5273271ba30bd77a922a6dd3
Raúl Caro Pastorino <dev@fryntiz.es>
2022-04-22 22:55:02 +0200

    Fix responsive and added new social links

--- src/components/SocialLinks.vue
+++ src/components/SocialLinks.vue
@@ -1,16 +1,116 @@
 <template>
-  <section class="flex items-center justify-center text-white bg-white">
-    <div class="flex flex-col bg-center bg-cover bg-no-repeat my-20">
+  <section class="bg-white py-20">
+    <div class="container px-8 mx-auto sm:px-12 lg:px-20">
+      <!--
+        <h1
+          class="text-sm font-bold tracking-wide text-center text-gray-800 uppercase mb-7"
+        >
+          Tecnologías Preferidas
+        </h1>
+        -->
       <div
-        class="grid place-items-center mx-auto my-3 bg-white rounded-3xl space-y-10"
+        class="
+          flex
+          grid
+          items-center
+          justify-center
+          grid-cols-4 grid-cols-12
+          gap-y-8
+        "
       >
-        <h1 class="text-5xl font-semibold text-blue-500">Redes Sociales</h1>
-        <div class="flex items-center justify-center space-x-3">
+        <div
+          class="
+            flex
+            items-center
+            justify-center
+            col-span-6
+            sm:col-span-4
+            md:col-span-3
+            xl:col-span-2
+          "
+        >
+          <a
+            href="https://www.linkedin.com/in/raulcaropastorino/"
+            target="_blank"
+            class="
+              background-blue
+              px-4
+              py-2
+              font-semibold
+              text-white
+              basis-1/2
+              md:basis-1/3
+              items-center
+              inline-flex
+              space-x-2
+              rounded
+            "
+          >
+            <svg
+              class="w-5 h-5 fill-current"
+              role="img"
+              viewBox="0 0 256 236"
+              xmlns="http://www.w3.org/2000/svg"
+              preserveAspectRatio="xMinYMin meet"
+            >
+              <path
+                d="M128.075 236.075l47.104-144.97H80.97l47.104 144.97z"
+                fill="#E24329"
+              />
+              <path
+                d="M128.075 236.074L80.97 91.104H14.956l113.119 144.97z"
+                fill="#FC6D26"
+              />
+              <path
+                d="M14.956 91.104L.642 135.16a9.752 9.752 0 0 0 3.542 10.903l123.891 90.012-113.12-144.97z"
+                fill="#FCA326"
+              />
+              <path
+                d="M14.956 91.105H80.97L52.601 3.79c-1.46-4.493-7.816-4.492-9.275 0l-28.37 87.315z"
+                fill="#E24329"
+              />
+              <path
+                d="M128.075 236.074l47.104-144.97h66.015l-113.12 144.97z"
+                fill="#FC6D26"
+              />
+              <path
+                d="M241.194 91.104l14.314 44.056a9.752 9.752 0 0 1-3.543 10.903l-123.89 90.012 113.119-144.97z"
+                fill="#FCA326"
+              />
+              <path
+                d="M241.194 91.105h-66.015l28.37-87.315c1.46-4.493 7.816-4.492 9.275 0l28.37 87.315z"
+                fill="#E24329"
+              />
+            </svg>
+            <span>Linkedin</span>
+          </a>
+        </div>
+        <div
+          class="
+            flex
+            items-center
+            justify-center
+            col-span-6
+            sm:col-span-4
+            md:col-span-3
+            xl:col-span-2
+          "
+        >
           <!-- Telegram -->
           <a
             href="https://t.me/FryntizOpenDev"
             target="_blank"
-            class="background-gray px-4 py-2 font-semibold text-white inline-flex items-center space-x-2 rounded"
+            class="
+              background-gray
+              px-4
+              py-2
+              font-semibold
+              text-white
+              inline-flex
+              items-center
+              space-x-2
+              rounded
+            "
           >
             <svg
               xmlns="http://www.w3.org/2000/svg"
@@ -42,12 +142,33 @@
 
             <span>Telegram</span>
           </a>
-
+        </div>
+        <div
+          class="
+            flex
+            items-center
+            justify-center
+            col-span-6
+            sm:col-span-4
+            md:col-span-3
+            xl:col-span-2
+          "
+        >
           <!-- Twitter -->
           <a
             href="https://twitter.com/fryntiz"
             target="_blank"
-            class="background-blue px-4 py-2 font-semibold text-white inline-flex items-center space-x-2 rounded"
+            class="
+              background-blue
+              px-4
+              py-2
+              font-semibold
+              text-white
+              inline-flex
+              items-center
+              space-x-2
+              rounded
+            "
           >
             <svg
               class="w-5 h-5 fill-current"
@@ -61,12 +182,33 @@
             </svg>
             <span>Twitter</span>
           </a>
-
+        </div>
+        <div
+          class="
+            flex
+            items-center
+            justify-center
+            col-span-6
+            sm:col-span-4
+            md:col-span-3
+            xl:col-span-2
+          "
+        >
           <!-- Gitlab -->
           <a
             href="https://gitlab.com/fryntiz"
             target="_blank"
-            class="background-yellow px-4 py-2 font-semibold text-white inline-flex items-center space-x-2 rounded"
+            class="
+              background-yellow
+              px-4
+              py-2
+              font-semibold
+              text-white
+              inline-flex
+              items-center
+              space-x-2
+              rounded
+            "
           >
             <svg
               class="w-5 h-5 fill-current"
@@ -105,6 +247,207 @@
               />
             </svg>
             <span>Gitlab</span>
+          </a>
+        </div>
+
+        <div
+          class="
+            flex
+            items-center
+            justify-center
+            col-span-6
+            sm:col-span-4
+            md:col-span-6
+            xl:col-span-2
+          "
+        >
+          <!-- GitHub -->
+          <a
+            href="https://github.com/fryntiz"
+            target="_blank"
+            class="
+              background-gray
+              px-4
+              py-2
+              font-semibold
+              text-white
+              inline-flex
+              items-center
+              space-x-2
+              rounded
+            "
+          >
+            <svg
+              class="w-5 h-5 fill-current"
+              role="img"
+              viewBox="0 0 256 236"
+              xmlns="http://www.w3.org/2000/svg"
+              preserveAspectRatio="xMinYMin meet"
+            >
+              <path
+                d="M128.075 236.075l47.104-144.97H80.97l47.104 144.97z"
+                fill="#E24329"
+              />
+              <path
+                d="M128.075 236.074L80.97 91.104H14.956l113.119 144.97z"
+                fill="#FC6D26"
+              />
+              <path
+                d="M14.956 91.104L.642 135.16a9.752 9.752 0 0 0 3.542 10.903l123.891 90.012-113.12-144.97z"
+                fill="#FCA326"
+              />
+              <path
+                d="M14.956 91.105H80.97L52.601 3.79c-1.46-4.493-7.816-4.492-9.275 0l-28.37 87.315z"
+                fill="#E24329"
+              />
+              <path
+                d="M128.075 236.074l47.104-144.97h66.015l-113.12 144.97z"
+                fill="#FC6D26"
+              />
+              <path
+                d="M241.194 91.104l14.314 44.056a9.752 9.752 0 0 1-3.543 10.903l-123.89 90.012 113.119-144.97z"
+                fill="#FCA326"
+              />
+              <path
+                d="M241.194 91.105h-66.015l28.37-87.315c1.46-4.493 7.816-4.492 9.275 0l28.37 87.315z"
+                fill="#E24329"
+              />
+            </svg>
+            <span>GitHub</span>
+          </a>
+        </div>
+
+        <div
+          class="
+            flex
+            items-center
+            justify-center
+            col-span-6
+            sm:col-span-4
+            md:col-span-6
+            xl:col-span-2
+          "
+        >
+          <!-- Facebook -->
+          <a
+            href="https://www.facebook.com/raupulus"
+            target="_blank"
+            class="
+              background-blue
+              px-4
+              py-2
+              font-semibold
+              text-white
+              inline-flex
+              items-center
+              space-x-2
+              rounded
+            "
+          >
+            <svg
+              class="w-5 h-5 fill-current"
+              role="img"
+              viewBox="0 0 256 236"
+              xmlns="http://www.w3.org/2000/svg"
+              preserveAspectRatio="xMinYMin meet"
+            >
+              <path
+                d="M128.075 236.075l47.104-144.97H80.97l47.104 144.97z"
+                fill="#E24329"
+              />
+              <path
+                d="M128.075 236.074L80.97 91.104H14.956l113.119 144.97z"
+                fill="#FC6D26"
+              />
+              <path
+                d="M14.956 91.104L.642 135.16a9.752 9.752 0 0 0 3.542 10.903l123.891 90.012-113.12-144.97z"
+                fill="#FCA326"
+              />
+              <path
+                d="M14.956 91.105H80.97L52.601 3.79c-1.46-4.493-7.816-4.492-9.275 0l-28.37 87.315z"
+                fill="#E24329"
+              />
+              <path
+                d="M128.075 236.074l47.104-144.97h66.015l-113.12 144.97z"
+                fill="#FC6D26"
+              />
+              <path
+                d="M241.194 91.104l14.314 44.056a9.752 9.752 0 0 1-3.543 10.903l-123.89 90.012 113.119-144.97z"
+                fill="#FCA326"
+              />
+              <path
+                d="M241.194 91.105h-66.015l28.37-87.315c1.46-4.493 7.816-4.492 9.275 0l28.37 87.315z"
+                fill="#E24329"
+              />
+            </svg>
+            <span>Facebook</span>
+          </a>
+        </div>
+
+        <div
+          class="
+            flex
+            items-center
+            justify-center
+            col-span-6
+            sm:col-span-4
+            md:col-span-6
+            xl:col-span-2
+          "
+        >
+          <!-- Instagram -->
+          <a
+            href="https://www.instagram.com/raupulus/"
+            target="_blank"
+            class="
+              bg-pink-600
+              px-4
+              py-2
+              font-semibold
+              text-white
+              inline-flex
+              items-center
+              space-x-2
+              rounded
+            "
+          >
+            <svg
+              class="w-5 h-5 fill-current"
+              role="img"
+              viewBox="0 0 256 236"
+              xmlns="http://www.w3.org/2000/svg"
+              preserveAspectRatio="xMinYMin meet"
+            >
+              <path
+                d="M128.075 236.075l47.104-144.97H80.97l47.104 144.97z"
+                fill="#E24329"
+              />
+              <path
+                d="M128.075 236.074L80.97 91.104H14.956l113.119 144.97z"
+                fill="#FC6D26"
+              />
+              <path
+                d="M14.956 91.104L.642 135.16a9.752 9.752 0 0 0 3.542 10.903l123.891 90.012-113.12-144.97z"
+                fill="#FCA326"
+              />
+              <path
+                d="M14.956 91.105H80.97L52.601 3.79c-1.46-4.493-7.816-4.492-9.275 0l-28.37 87.315z"
+                fill="#E24329"
+              />
+              <path
+                d="M128.075 236.074l47.104-144.97h66.015l-113.12 144.97z"
+                fill="#FC6D26"
+              />
+              <path
+                d="M241.194 91.104l14.314 44.056a9.752 9.752 0 0 1-3.543 10.903l-123.89 90.012 113.119-144.97z"
+                fill="#FCA326"
+              />
+              <path
+                d="M241.194 91.105h-66.015l28.37-87.315c1.46-4.493 7.816-4.492 9.275 0l28.37 87.315z"
+                fill="#E24329"
+              />
+            </svg>
+            <span>Instagram</span>
           </a>
         </div>
       </div>
